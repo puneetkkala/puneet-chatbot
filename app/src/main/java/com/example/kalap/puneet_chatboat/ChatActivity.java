8be60916b472cbf7ebc505da81fe18d54da7225b
Puneet Kala <Puneet Kala>
2016-02-27 13:29:16 +0530

    creation commit

---
+++ app/src/main/java/com/example/kalap/puneet_chatboat/ChatActivity.java
@@ -0,0 +1,18 @@
+package com.example.kalap.puneet_chatboat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+    public void sendMessage(View view){
+        
+    }
+}
