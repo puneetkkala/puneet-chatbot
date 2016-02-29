6f4cb482ed8d7f633d075f65e1263953b858607d
Puneet Kala <Puneet Kala>
2016-02-29 16:05:40 +0530

    added offline messages feature

--- app/src/main/java/com/example/kalap/puneet_chatboat/ChatActivity.java
+++ app/src/main/java/com/example/kalap/puneet_chatboat/ChatActivity.java
@@ -1,10 +1,14 @@
 package com.example.kalap.puneet_chatboat;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -24,15 +28,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class ChatActivity extends AppCompatActivity {
 
     private List<BotAppMessage> messageList;
     private MessageAdapter messageAdapter;
+    private SharedPreferences preferences;
     EditText getText;
     private BotMessageDbHelper messageDbHelper;
     private SQLiteDatabase database;
+    private Set<String> offline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +64,16 @@ public class ChatActivity extends AppCompatActivity {
         sendMessage.setOnClickListener(new ImageButton.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendMessage(view);
+                if(isConnected()){
+                    Snackbar snackbar = Snackbar.make(view,"Processing offline messages",Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    processOffline();
+                    sendMessage();
+                } else {
+                    Snackbar snackbar = Snackbar.make(view,"You are offline",Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    storeOffline();
+                }
             }
         });
 
@@ -91,6 +109,12 @@ public class ChatActivity extends AppCompatActivity {
         }
     }
 
+    public boolean isConnected(){
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
     public void addToDatabase(BotAppMessage bam){
         database = messageDbHelper.getWritableDatabase();
 
@@ -98,14 +122,10 @@ public class ChatActivity extends AppCompatActivity {
         values.put(BotMessageDbHelper.COLUMN_TYPE, bam.getType());
         values.put(BotMessageDbHelper.COLUMN_CONTENT, bam.getContent());
 
-        database.insert(BotMessageDbHelper.TABLE_NAME,null,values);
+        database.insert(BotMessageDbHelper.TABLE_NAME,null, values);
     }
 
-    public void sendMessage(View view) {
-
-//        ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-//        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-//        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    public void sendMessage() {
         RequestQueue queue = Volley.newRequestQueue(this);
         BotAppMessage appMessage = new BotAppMessage(getText.getText().toString(),"S");
         messageList.add(appMessage);
@@ -139,5 +159,63 @@ public class ChatActivity extends AppCompatActivity {
         );
         queue.add(jsonObjectRequest);
         getText.setText("");
+    }
+
+    public void storeOffline(){
+        preferences = getSharedPreferences("offline", MODE_PRIVATE);
+        offline = preferences.getStringSet("offline", new HashSet<String>());
+        offline.add(getText.getText().toString());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putStringSet("offline", offline);
+        editor.commit();
+        BotAppMessage appMessage = new BotAppMessage(getText.getText().toString(),"O");
+        messageList.add(appMessage);
+        messageAdapter.notifyDataSetChanged();
+        getText.setText("");
+    }
+
+    public void processOffline(){
+        preferences = getSharedPreferences("offline", MODE_PRIVATE);
+        offline = preferences.getStringSet("offline", new HashSet<String>());
+        RequestQueue queue = Volley.newRequestQueue(this);
+        Iterator<String> iterator = offline.iterator();
+        while(!iterator.hasNext() && isConnected()){
+            String content = iterator.next();
+            BotAppMessage appMessage = new BotAppMessage(content,"S");
+            messageList.add(appMessage);
+            messageAdapter.notifyDataSetChanged();
+            addToDatabase(appMessage);
+            String url = "http://www.personalityforge.com/api/chat/?apiKey=6nt5d1nJHkqbkphe&chatBotID=63906" +
+                    "&message=" + appMessage.getContent() + "&externalID=chirag1";
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            BotAppMessage appMessage1;
+                            JSONObject jsonObject;
+                            try {
+                                jsonObject = response.getJSONObject("message");
+                                appMessage1 = new BotAppMessage(jsonObject.getString("message"),"R");
+                                messageList.add(appMessage1);
+                                messageAdapter.notifyDataSetChanged();
+                                addToDatabase(appMessage1);
+                            } catch (JSONException e) {
+                                Log.i("BOTREPLY", e.getMessage());
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.i("BOTREPLY", error.getMessage());
+                        }
+                    }
+            );
+            queue.add(jsonObjectRequest);
+            offline.remove(content);
+        }
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putStringSet("offline", offline);
+        editor.commit();
     }
 }
